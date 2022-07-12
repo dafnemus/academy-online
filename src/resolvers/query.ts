475ec88dd1cbcb0a3e09174ad005f7c44e9cc58f
dafnemus <dafnemus@gmail.com>
2022-07-12 15:46:21 -0300

    student query is added

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -6,6 +6,19 @@ const query: IResolvers = {
     students(): any {
       return database.students;
     },
+    student(__: void, { id }): any {
+      const student = database.students.filter(student => student.id === id)[0];
+
+      if(student === undefined) {
+        return {
+          id: '-1',
+          name: `Student with id ${id} not found`,
+          email: '',
+          courses: [],
+        }
+      }
+      return student;
+    }
   },
 };
 
