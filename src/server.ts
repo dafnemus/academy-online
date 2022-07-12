608810983cc6de01604f6d616f0bb64a64f45eaf
dafnemus <dafnemus@gmail.com>
2022-07-12 12:05:36 -0300

    configured basic server http

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import compression from 'compression';
+import cors from 'cors';
+import { createServer } from 'http';
+
+const app = express();
+
+app.use(cors());
+app.use(compression());
+
+app.get('/', (req, res) => {
+  res.send('Hola')
+})
+
+const PORT = 5200
+
+const httpServer = createServer(app);
+httpServer.listen(
+  {
+    port: PORT
+  },
+  () => console.log(`Listening in http://localhost:${PORT}`)
+);
