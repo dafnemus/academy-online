f9c08fbc6cd793e2220427cb447fd7c70aaa34f4
dafnemus <dafnemus@gmail.com>
2022-07-12 13:03:57 -0300

    - modified level type from Course in schema.graphql - added config apolloServer in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,41 @@
 import express from 'express';
 import compression from 'compression';
+import { ApolloServer } from 'apollo-server-express';
 import cors from 'cors';
 import { createServer } from 'http';
+import expressPlayGround from 'graphql-playground-middleware-express';
+import schema from './schema';
 
 const app = express();
 
 app.use(cors());
 app.use(compression());
 
-app.get('/', (req, res) => {
-  res.send('Hola');
-});
+let server = null;
+
+const startServer = async () => {
+  server = new ApolloServer({
+    schema,
+    introspection: true,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+};
+
+app.get(
+  '/',
+  expressPlayGround({
+    endpoint: '/graphql',
+  })
+);
 
 const PORT = 5200;
-
+startServer();
 const httpServer = createServer(app);
 
 httpServer.listen(
   {
     port: PORT,
   },
-  () => console.log(`Listening in http://localhost:${PORT}`)
+  () => console.log(`Listening in http://localhost:${PORT}/graphql`)
 );
