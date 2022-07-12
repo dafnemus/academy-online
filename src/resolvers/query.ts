895ede9d33083918b1ae5557ee4cd9e348329994
dafnemus <dafnemus@gmail.com>
2022-07-12 18:31:10 -0300

    get course by id

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -24,6 +24,22 @@ const query: IResolvers = {
     courses(): any {
       return database.courses;
     },
+    course(__: void, { id }): any {
+      const course = database.courses.filter((course) => course.id === id)[0];
+      if(course === undefined) {
+        return {
+          id: '-1',
+          title: `Course with id ${id} not found`,
+          description: '',
+          class: '',
+          time: '',
+          logo: '',
+          level: '',
+          path: '',
+        }
+      }
+      return course
+    },
   },
 };
 
