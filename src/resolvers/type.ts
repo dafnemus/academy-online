7ad44ca786e2a3d94bc7eb1e9d9e274099782949
dafnemus <dafnemus@gmail.com>
2022-08-17 16:31:34 -0300

    Dafne Musante | Courses-students - get students into Course by id query

--- src/resolvers/type.ts
+++ src/resolvers/type.ts
@@ -5,11 +5,23 @@ import _ from 'lodash';
 const type: IResolvers = {
   Student: {
     courses: (parent) => {
-      const listCourses: Array<any> = [];
+      const coursesList: Array<any> = [];
       parent.courses.map((course: string) => {
-        listCourses.push(_.filter(database.courses, ['id', course])[0]);
+        coursesList.push(_.filter(database.courses, ['id', course])[0]);
       });
-      return listCourses;
+      return coursesList;
+    },
+  },
+  Course: {
+    students: (parent) => {
+      const studentsList: Array<any> = [];
+      const studentId = parent.id;
+      database.students.map((student: any) => {
+        if (student.courses.filter((id: any) => id === studentId) > 0) {
+          studentsList.push(student);
+        }
+      });
+      return studentsList;
     },
   },
 };
